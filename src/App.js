fe1dff4fbf9a72d7b17e2e55f5893f69571c6ddf
Brian J. Strout <brianjstrout@gmail.com>
2022-10-04 21:26:06 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import BusinessCard from './components/businesscard';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hi there</h1>
+    </div>
+
+  );
+
+}
+
+export default App;
---
+++ src/components/businesscard.js
@@ -0,0 +1,13 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+
+export default function BusinessCard(){
+
+    return(
+<div>
+    <h1>hi!</h1>
+</div>
+  
+    )
+}
